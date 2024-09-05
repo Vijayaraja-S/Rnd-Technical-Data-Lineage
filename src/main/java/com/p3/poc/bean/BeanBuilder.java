1cc40bfa67c382828ae90e808ba1087b5c188aaa
vijayaraja <vijayaraja.s@platform3solutions.com>
2024-09-05 16:20:44 +0530

    Existing code refactoring

--- src/main/java/com/p3/poc/bean/BeanBuilder.java
+++ src/main/java/com/p3/poc/bean/BeanBuilder.java
@@ -8,14 +8,17 @@ import java.io.IOException;
 
 @Slf4j
 public class BeanBuilder {
-  public static InputBean buildInputBean(String configFile) throws IOException {
-    log.error("Path : {}", configFile);
-    try (FileInputStream input = new FileInputStream(configFile)) {
-      String yamlString = new String(input.readAllBytes());
-      return new Yaml().loadAs(yamlString, InputBean.class);
-    } catch (IOException exception) {
-      log.error("Exception : {}", exception.getMessage(), exception);
-      throw exception;
+    private BeanBuilder() {
     }
-  }
+
+    public static InputBean buildInputBean(String configFile) throws IOException {
+        log.error("Path : {}", configFile);
+        try (FileInputStream input = new FileInputStream(configFile)) {
+            String yamlString = new String(input.readAllBytes());
+            return new Yaml().loadAs(yamlString, InputBean.class);
+        } catch (IOException exception) {
+            log.error("Exception : {}", exception.getMessage(), exception);
+            throw exception;
+        }
+    }
 }
