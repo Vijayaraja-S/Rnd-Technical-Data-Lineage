f3bbc7cd37ac63eda767b9e637436de6b276ec45
vijayaraja <vijayaraja.s@platform3solutions.com>
2024-09-13 20:04:09 +0530

    query transform

--- src/main/java/com/p3/poc/Main.java
+++ src/main/java/com/p3/poc/Main.java
@@ -7,9 +7,13 @@ import com.p3.poc.chart.SunburstChartHelper;
 import com.p3.poc.parser.SQLParserApplication;
 import com.p3.poc.parser.bean.GlobalCollector;
 import com.p3.poc.parser.parsing.exception.InvalidStatement;
+import io.trino.jdbc.$internal.jackson.core.JsonProcessingException;
 import org.json.JSONArray;
 
 import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
@@ -23,5 +27,26 @@ public class Main {
         final SunburstChartHelper sunburstChartHelper = new SunburstChartHelper();
         final JSONArray objects = sunburstChartHelper.generateSunburst();
         System.out.println(objects);
+//        final String s = convertToExecutableQuery(inputBean.getSqlQuery());
+//        System.out.println(s);
+
+    }
+
+    public static String convertToExecutableQuery(String query) throws   JsonProcessingException {
+        Pattern fieldPattern = Pattern.compile(RegexConstant.INPUT_REPLACER_REGEX, Pattern.MULTILINE);
+        Pattern verificationPattern =
+                Pattern.compile(RegexConstant.INPUT_FIELD_REGEX, Pattern.MULTILINE);
+        Matcher fieldMatcher = fieldPattern.matcher(query);
+        Map<String, String> matcherString = new HashMap<>();
+        while (fieldMatcher.find()) {
+            String group = fieldMatcher.group(0);
+            if (!group.trim().isEmpty() && verificationPattern.matcher(group.trim()).find()) {
+                matcherString.put("@@" + group + "@@", "");
+            }
+        }
+        for (Map.Entry<String, String> stringStringEntry : matcherString.entrySet()) {
+            query = query.replace(stringStringEntry.getKey(), stringStringEntry.getValue());
+        }
+        return query;
     }
 }
---
+++ src/main/java/com/p3/poc/RegexConstant.java
@@ -0,0 +1,32 @@
+package com.p3.poc;
+
+public class RegexConstant {
+  public static final String VALIDATE_NAME_REGEX = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,})";
+  public static final String EMAIL_PATTERN_REGEX =
+      "^([\\w-]+(?:\\.[\\w-]+)*)@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([A-Za-z]{2,6}(?:\\.[A-Za-z]{2,6})?)$";
+  public static final String NAME_PATTERN_REGEX = "^[a-zA-Z0-9_]{5,30}$";
+  public static final String PATTERN_NAME_REGEX = "^[a-zA-Z_]{1,30}$";
+  public static final String FIELD_NAME_REGEX = "^[a-zA-Z]\\w*$";
+  public static final String VALID_NAME_REGEX =
+      "^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$).{8,15}$";
+  public static final String HOLD_NAME_REGEX = "\\/\\w+(\\/)?$";
+  public static final String RETEN_NAME_REGEX = "\\/\\w+(\\/)?$";
+  public static final String VALIDATION_REGEX =
+      "/^[^* | \\ \" : < > [ ] { } ` \\ ( ) '' ; @ . _ - & $]+$/";
+  public static final String LAST_NAME_REGEX =
+      "/^[^* | \\ \" : < > [ ] { } ` \\ ( ) '' ; @ . _ - & $]+$/";
+  public static final String UPDATE_SPARK_REGEX = "(?<=message\":\").*(?=\",\"error)";
+  public static final String INPUT_REPLACER_REGEX = "(?<=@@)([\\S\\s]*?)(?=@@)";
+  public static final String INPUT_FIELD_REGEX = "\\$\\{(.*?)}";
+  public static final String ERROR_REGEX = "(?s).*(?=line\\s*\\d+:\\d+:\\s*)";
+  public static final String REGEX = "line\\s*\\d+:\\d+:\\s*";
+
+
+
+
+
+
+
+
+ 
+}
